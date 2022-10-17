5099f705fa0c3540063935c0738716d30d5577f5
susankhas <susankhas81@gmail.com>
2022-10-17 02:42:05 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 *
 * Return Always 0 (Succes)
 */
-int main(void)/* main will not return any value*/
+int main(void)/*Return:function will not return any value */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
